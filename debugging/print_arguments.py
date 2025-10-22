035cc58006b258bcc731efcfe4635f29825c2638
natahaciyeva <natiagadjieva@gmai.com>
2025-10-22 14:50:41 +0000

    factorial.py

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import sys
 
-def main():
-    for arg in sys.argv[1:]:
-        print(arg)
-
-if __name__ == "__main__":
-    main()
+for arg in sys.argv[1:]:
+    print(arg)
